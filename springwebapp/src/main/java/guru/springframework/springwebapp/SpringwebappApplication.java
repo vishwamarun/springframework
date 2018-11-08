695b65eea657165a1b90e3b105f9f2d0c0f6c78e
Arun <arunkumarvishwam@gmail.com>
2018-11-08 15:40:00 +0800

    First Commit

---
+++ springwebapp/src/main/java/guru/springframework/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+}
